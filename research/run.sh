2880032698389c6d254a6279e54a64218264cfd6
I-Sun <35124129+I-Sun@users.noreply.github.com>
2018-11-21 22:54:50 +0800

    Update run.sh

--- research/run.sh
+++ research/run.sh
@@ -11,7 +11,7 @@ export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 # 定义各目录
 output_dir=/output  # 训练目录
 # todo 修改数据路径（tfrecord）
-dataset_dir=/data/insun/quiz-w8-data # 数据集目录，这里是写死的，记得修改
+dataset_dir=/data/insun/udacity # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train     # 训练目录 固定的输出目录是 /output
 checkpoint_dir=$train_dir       # 保存check point 的目录
@@ -19,7 +19,7 @@ eval_dir=$output_dir/eval       # 预测的目录
 
 # config文件
 # todo 修改自己的config文件的名称
-config=ai100_quiz_w8.config
+config=ssd_mobilenet_v1_udacity.config
 pipeline_config_path=$output_dir/$config
 
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
@@ -40,7 +40,7 @@ do
     python ./object_detection/train.py --train_dir=$train_dir --pipeline_config_path=$pipeline_config_path
 
     echo "############" $i "evaluating, this takes a long while #################"
-    python ./object_detection/eval.py --checkpoint_dir=$checkpoint_dir --eval_dir=$eval_dir --pipeline_config_path=$pipeline_config_path
+#   python ./object_detection/eval.py --checkpoint_dir=$checkpoint_dir --eval_dir=$eval_dir --pipeline_config_path=$pipeline_config_path
 done
 
 # 导出模型
